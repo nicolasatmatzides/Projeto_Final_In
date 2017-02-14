fa2e0df291278c991bbd992be5e093d0946e600b
Nicolas Atmatzides <nicolas_atmatzides@id.uff.br>
2017-02-14 19:07:19 +0000

    editor de texto

--- ProjetoIN/db/schema.rb
+++ ProjetoIN/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213181528) do
+ActiveRecord::Schema.define(version: 20170214171523) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "content"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170213181528) do
     t.text     "description"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "counter"
   end
 
 end
